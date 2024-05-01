6b47695cf663ea1d37a3b857bc867d5126e51f3a
Sean Bogolin <SeanBo@marvin.com>
2024-04-30 22:05:35 -0500

    Create UseGames Hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-	baseURL: 'https://api.rawg.io/api/',
+	baseURL: 'https://api.rawg.io/api',
 	params: {
 		key: '2e1ec262bc4140ea919b81413c5c1137',
 	},
